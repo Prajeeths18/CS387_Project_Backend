7894dad3a640414fb5dc6df5d9d49e3791c8e9c1
girish-srivatsa <girish.srivatsa29@gmail.com>
2022-04-29 15:51:26 +0530

    added auth patch

--- backend/models/user.auth.js
+++ backend/models/user.auth.js
@@ -7,7 +7,7 @@ async function login(username, password) {
         SELECT user_id,username,password,role,valid FROM gen_user WHERE username=$1;
     `
     let user = await db.query(query,[username]).catch(e=>e).then(x=>x);
-    if(!user) {
+    if(!user || (!user.rows)) {
         return null;
     } else {
         user = user.rows[0];
